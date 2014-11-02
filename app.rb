1032f83a42b7174351e9e12872e9b2a6c11a297c
KOSK <mail@kosk.me>
2014-11-02 13:52:11 +0900

    Simple API

--- app.rb
+++ app.rb
@@ -7,6 +7,27 @@ require 'sinatra/json'
 
 ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'sqlite3://localhost/db/development.sqlite')
 
-get '/' do
-  "hello"
+class User < ActiveRecord::Base
+end
+
+get '/status/all' do
+  users = User.select(:username, :status, :updated_at)
+
+  if users.present?
+    json users
+  else
+    status 404
+  end
+end
+
+post '/status/update' do
+  user = User.find_by(username: params[:username])
+
+  if user.present?
+    user.status = params[:status]
+    user.save
+    json user
+  else
+    status 404
+  end
 end
